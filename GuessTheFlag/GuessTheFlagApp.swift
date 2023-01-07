6ca194aa7dcaad7b36d553302728503b1201aac1
Maurício Costa <86126994+mauriciomunky@users.noreply.github.com>
2023-01-06 21:42:03 -0300

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Maurício Costa on 06/01/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
